02a43349b1eb3d0e40f0cd887abfe68c1b1cd118
Nestor Torres <alu0101021768@ull.edu.es>
2021-04-06 11:03:43 +0100

    Arreglando fallo en la clase seed, no iban los macros

--- src/seed.ts
+++ src/seed.ts
@@ -2,7 +2,7 @@ import { Food } from './food';
 import { macroType, locationType } from './helpers';
 
 export class Seed extends Food {
-  constructor(name: string, origin: locationType, macros: macroType[],
+  constructor(name: string, origin: locationType, macros: macroType,
     weight: number, kcalPer100gr: number, pricePerKg: number) {
     super(name, origin, macros, weight, kcalPer100gr, pricePerKg);
   }
