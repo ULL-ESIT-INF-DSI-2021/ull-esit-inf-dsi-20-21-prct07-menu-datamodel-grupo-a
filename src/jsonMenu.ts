b524de4de91bf1b08e76338d31b8671d6d0cac42
Nestor Torres <alu0101021768@ull.edu.es>
2021-04-17 22:07:53 +0100

    Adding jsonMenu, jsonPlate and jsonifyCommand

---
+++ src/jsonMenu.ts
@@ -0,0 +1,15 @@
+import { Menu } from './menu';
+import { jsonPlate } from './jsonPlate';
+
+export class jsonMenu {
+  public static getJsonMenu(menu: Menu): Object {
+    return {
+      name: menu.getName(),
+      plates: menu.getPlates().map((plate) => jsonPlate.getJsonPlate(plate)),
+      kcal: menu.getKcal(),
+      totalPrice: menu.getTotalPrice(),
+      mainGroup: menu.getMainGroup(),
+      foodGroupList: menu.getFoodGroupList(),
+    };
+  }
+}
---
+++ src/jsonifyCommand.ts
@@ -0,0 +1,40 @@
+import { Menu } from './menu';
+import { Plate } from './plate';
+import { jsonMenu } from './jsonMenu';
+import { jsonPlate } from './jsonPlate';
+import lowdb from 'lowdb';
+import FileSync from 'lowdb/adapters/FileSync';
+
+type schemaType = {
+  userCommand: {
+    menus: Object[],
+    plates: Object[]
+  }
+}
+
+export class dbManager {
+  private database: lowdb.LowdbSync<schemaType>;
+
+  constructor(private username: string, command: Command = {}) {
+    this.database = lowdb(new FileSync('Command.json'));
+    if (this.database.has('command').value()) {
+      const dbCommand = this.database.get('userCommand').value();
+    }
+  }
+
+  public menuCollectionToJson(selectedMenus: Menu[]): Object[] {
+    return selectedMenus.map((menu) => jsonMenu.getJsonMenu(menu));
+  }
+
+  public plateCollectionToJson(selectedPlates: Plate[]): Object[] {
+    return selectedPlates.map((plate) => jsonPlate.getJsonPlate(plate));
+  }
+
+  public setMenuCollection(selectedMenus: Menu[]) {
+    this.menuJsonCollection = this.menuCollectionToJson(selectedMenus);
+  }
+
+  public setPlatesCollection(selectedPlates: Plate[]) {
+    this.platesJsonCollection = this.plateCollectionToJson(selectedPlates);
+  }
+}
