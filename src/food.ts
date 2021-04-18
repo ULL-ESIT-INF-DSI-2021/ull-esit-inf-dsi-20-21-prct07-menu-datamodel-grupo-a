99024b9ab2f99028d2336888eba7e9dd0cd5e094
Nestor Torres <alu0101021768@ull.edu.es>
2021-04-18 02:59:48 +0100

    Starting the documentation

--- src/food.ts
+++ src/food.ts
@@ -1,76 +1,129 @@
 import { macroType, locationType, Printable, Identifiable} from './helpers';
 
+/**
+ * @description Class that handles Food, that implements Printable and
+ * Identifiable interfaces.
+ */
 export abstract class Food implements Printable, Identifiable {
+  /**
+   * @description Food class constructor
+   * @param name Consists of a string
+   * @param origin Consists of a locationType
+   * @param macros Consists of a macroType
+   * @param weight Consists of a number
+   * @param kcalPer100gr Consists of a number
+   * @param pricePerKg Consists of a number
+   */
   constructor(private name: string, private origin: locationType,
     private macros: macroType, private weight: number,
     private kcalPer100gr: number, private pricePerKg: number) {
 
   }
 
-  // Getters & setters
-
+  /**
+   * @description Getter for the name
+   * @returns Returns a string
+   */
   getName(): string {
     return this.name;
   }
-
+  /**
+   * @description Getter for the origin
+   * @returns Returns a locationType
+   */
   getOrigin(): locationType {
     return this.origin;
   }
-
+  /**
+   * @description Getter for the macros
+   * @returns Returns  macroType
+   */
   getMacros(): macroType {
     return this.macros;
   }
-
+  /**
+   * @description Getter for the weight
+   * @returns Returns a number
+   */
   getWeight(): number {
     return this.weight;
   }
-
+  /**
+   * @description Getter for the kcal per 100 gr
+   * @returns Returns a number
+   */
   getKcalPer100gr(): number {
     return this.kcalPer100gr;
   }
-
+  /**
+   * @description Getter for the price per kg
+   * @returns Returns a number
+   */
   getPricePerKg(): number {
     return this.pricePerKg;
   }
-
+  /**
+   * @description Setter for the name
+   * @param name Consists of a string
+   */
   setName(name: string) {
     this.name = name;
   }
-
+  /**
+   * @description Setter for the origin
+   * @param origin Consists of a locationType
+   */
   setOrigin(origin: locationType) {
     this.origin = origin;
   }
-
+  /**
+   * @description Setter for the macros
+   * @param macros Consists of a macroType
+   */
   setMacros(macros: macroType) {
     this.macros = macros;
   }
-
+  /**
+   * @description Setter for the weight
+   * @param weight Consists of a number
+   */
   setWeight(weight: number) {
     this.weight = weight;
   }
-
+  /**
+   * @description Setter for the kcal per 100gr
+   * @param kcal Consists of a number
+   */
   setKcalPer100gr(kcal: number) {
     this.kcalPer100gr = kcal;
   }
-
+  /**
+   * @description Setter for the price per kg
+   * @param price Consists of a number
+   */
   setPricePerKg(price: number) {
     this.pricePerKg = price;
   }
-
-  // Methods
-
+  /**
+   * @description Method that calculates the price per weight
+   * @returns Returns a number
+   */
   calcPricePerWeigth(): number {
     // Assuming that the weight is setted as grams, 1kg equals 1000gr
     return (this.getWeight() / 1000) * this.getPricePerKg();
   }
-
+  /**
+   * @description Function that calculates the total kcal of the food
+   * @returns Returns a number
+   */
   calcKcal(): number {
     // Assuming that we got the kcals per 100g
     return (this.getWeight() * this.getKcalPer100gr()) / 100;
   }
-
-  // Interface methods
-
+  /**
+   * @description Method that prints all the food information
+   * @returns Returns a string
+   */
   print() {
     return 'Class: ' + this.getClassName() +
       '\nName: ' + this.getName() +
@@ -82,7 +135,10 @@ export abstract class Food implements Printable, Identifiable {
       '\nKcal per 100 gr: ' + this.getKcalPer100gr() +
       '\nPrice per kg: ' + this.getPricePerKg();
   }
-
+  /**
+   * @description Getter for the class name
+   * @returns Returns a string
+   */
   getClassName(): string {
     return this.constructor.name;
   }
--- src/menu.ts
+++ src/menu.ts
@@ -1,7 +1,7 @@
 import { Plate } from './plate';
 
 /**
- * Class that handles a Menu, made out of different plates.
+ * @description Class that handles a Menu, made out of different plates.
  */
 export class Menu {
   private kcal: number;
@@ -9,16 +9,27 @@ export class Menu {
   private mainGroup: string;
   private foodGroupList: string[];
 
-
+  /**
+   * @description Class menu constructor
+   * @param name Consists of a string
+   * @param plates Consists of a plate array
+   */
   constructor(private name: string, private plates: Plate[]) {
     this.update();
   }
-
+  /**
+   * @description Method that adds a plate to a menu
+   * @param newPlate Consists of a plate
+   */
   public add(newPlate: Plate) {
     this.getPlates().push(newPlate);
     this.update();
   }
-
+  /**
+   * @description Method that removes a plate from a menu
+   * @param newPlate Consists of a plate
+   * @returns Returns a boolean
+   */
   public remove(newPlate: Plate): boolean {
     if (this.getPlates().includes(newPlate)) {
       this.getPlates().splice(this.getPlates().indexOf(newPlate), 1);
@@ -27,7 +38,10 @@ export class Menu {
     }
     return false;
   }
-
+  /**
+   * @description Method that sets all the menu values after it
+   * checks the validity of the menu
+   */
   private update() {
     if (!this.plateValidityCheck(this.plates)) {
       throw new Error('Either the number of plates or the amount' +
@@ -38,7 +52,11 @@ export class Menu {
     this.setFoodGroupList(this.getGroupList());
     this.setMainGroup(this.calcMainGroup());
   }
-
+  /**
+   * @description Method that checks the validity of the menu
+   * @param plates Consists of a plate array
+   * @returns Returns a boolean
+   */
   private plateValidityCheck(plates: Plate[]): boolean {
     if (plates.length < 3) {
       return false;
@@ -55,55 +73,94 @@ export class Menu {
     }
     return true;
   }
-
+  /**
+   * @description Getter for the name
+   * @returns Returns a string
+   */
   public getName(): string {
     return this.name;
   }
-
+  /**
+   * @description Setter for the name
+   * @param name Consists of a string
+   */
   public setName(name: string): void {
     this.name = name;
   }
-
+  /**
+   * @description Getter for the plates
+   * @returns Returns a plate array
+   */
   public getPlates(): Plate[] {
     return this.plates;
   }
-
+  /**
+   * @description Setter for the plates
+   * @param plates Consists of a plate array
+   */
   public setPlates(plates: Plate[]): void {
     this.plates = plates;
   }
-
+  /**
+   * @description Getter for the kcal
+   * @returns Returns a number
+   */
   public getKcal(): number {
     return this.kcal;
   }
-
+  /**
+   * @description Setter for the kcal
+   * @param kcal Consists of a number
+   */
   public setKcal(kcal: number): void {
     this.kcal = kcal;
   }
-
+  /**
+   * @description Method that returns the total menu price
+   * @returns Returns a number
+   */
   public getTotalPrice(): number {
     return this.totalPrice;
   }
-
+  /**
+   * @description Setter for the total price
+   * @param totalPrice Consists of a number
+   */
   public setTotalPrice(totalPrice: number): void {
     this.totalPrice = totalPrice;
   }
-
+  /**
+   * @description Getter for the menu main group
+   * @returns Returns a string
+   */
   public getMainGroup(): string {
     return this.mainGroup;
   }
-
+  /**
+   * @description Setter for the menu main group
+   * @param mainGroup Consists of a string
+   */
   public setMainGroup(mainGroup: string): void {
     this.mainGroup = mainGroup;
   }
-
+  /**
+   * @description Getter for the menu group list
+   * @returns Returns a string array
+   */
   public getFoodGroupList(): string[] {
     return this.foodGroupList;
   }
-
+  /**
+   * @description Setter for the menu group list
+   * @param foodGroupList Consists of a string array
+   */
   public setFoodGroupList(foodGroupList: string[]): void {
     this.foodGroupList = foodGroupList;
   }
-
+  /**
+   * @description Method that calculates all the kcal in the menu
+   * @returns Returns a number
+   */
   public calcKcal(): number {
     let result: number = 0;
     this.getPlates().forEach((value) => {
@@ -111,7 +168,10 @@ export class Menu {
     });
     return result;
   }
-
+  /**
+   * @description Method that calculates the total price of the menu
+   * @returns Returns a number
+   */
   public calcTotalPrice(): number {
     let result: number = 0;
     this.getPlates().forEach((value) => {
@@ -119,7 +179,10 @@ export class Menu {
     });
     return result;
   }
-
+  /**
+   * @description Getter for the menu group list
+   * @returns Returns a string array
+   */
   public getGroupList(): string[] {
     const result = new Set<string>();
     this.getPlates().forEach((value) => {
@@ -129,7 +192,10 @@ export class Menu {
     });
     return Array.from(result.values());
   }
-
+  /**
+   * @description Method that calculates the food main group of the menu
+   * @returns Returns a string
+   */
   public calcMainGroup(): string {
     const groupList: string[] = this.getGroupList();
     const result = groupList.sort((a, b) => {
